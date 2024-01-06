85682badeb16b7ad5b96a83a0277649e05797f65
engWafula <wafulaallan5@gmail.com>
2024-01-06 16:56:19 +0300

    adding bug fixes

--- src/app/page.js
+++ src/app/page.js
@@ -15,19 +15,16 @@ export default function Home() {
         />
         <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
           <p>
-          Welcome to Genix's Pastry, where passion for baking meets culinary artistry. At Genix's, we believe that every bite should be a delightful experience, and our commitment to crafting exceptional pastries reflects our dedication to this philosophy.
-
-Founded with a vision to redefine the pastry experience, Genix's is more than just a restaurant; it's a celebration of flavors, textures, and the art of baking. Our team of skilled pastry chefs combines traditional techniques with innovative ideas to create a diverse menu that caters to every palate.          </p>
+            Welcome to Genix&apos;s Pastry, where passion for baking meets culinary artistry. At Genix&apos;s, we believe that every bite should be a delightful experience, and our commitment to crafting exceptional pastries reflects our dedication to this philosophy.
+          </p>
           <p>
-          Customer satisfaction is our top priority, and we take pride in delivering an unparalleled level of service. Our knowledgeable and friendly staff is always ready to assist you in choosing the perfect pastry or dessert to suit your cravings.
-
-
+            Customer satisfaction is our top priority, and we take pride in delivering an unparalleled level of service. Our knowledgeable and friendly staff is always ready to assist you in choosing the perfect pastry or dessert to suit your cravings.
           </p>
         </div>
       </section>
       <section className="text-center my-8" id="contact">
         <SectionHeaders
-          subHeader={'Don\'t hesitate'}
+          subHeader={"Don't hesitate"}
           mainHeader={'Contact us'}
         />
         <div className="mt-8">
@@ -37,5 +34,5 @@ Founded with a vision to redefine the pastry experience, Genix's is more than ju
         </div>
       </section>
     </>
-  )
+  );
 }
